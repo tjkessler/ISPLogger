671a7848712bac8dd84a5b8958caa9325be3865a
tjkessler <travis.j.kessler@gmail.com>
2019-05-19 21:46:06 -0400

    Version bump: 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='isplogger',
-    version='0.1.0',
+    version='0.2.0',
     description='A tool for monitoring/recording internet access',
     url='http://github.com/tjkessler/isplogger',
     author='Travis Kessler',
